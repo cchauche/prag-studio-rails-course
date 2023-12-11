c34b384ea51d8a13882614a51bc4f02296b35acf
Colin Chauche <colin.chauche@gmail.com>
2023-12-11 15:09:02 -0600

    complete section 10 video

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "events" => "events#index"
+  get "events/:id" => "events#show"
 end
