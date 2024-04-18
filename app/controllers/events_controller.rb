35178a298d9ca0b3a9c9b9b0820f8834527a9e59
Colin Chauche <colin.chauche@gmail.com>
2024-04-18 09:28:44 -0600

    complete section 15 video

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,11 +23,17 @@ class EventsController < ApplicationController
   end
 
   def create
-
     @event = Event.new(event_params)
     @event.save()
 
     redirect_to @event
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to events_url, status: :see_other
   end
 
   private
