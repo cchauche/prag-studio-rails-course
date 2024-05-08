d8350b0fc2d00633bcbe7c139e306dcf3f953c18
Colin Chauche <colin.chauche@gmail.com>
2024-05-07 20:06:41 -0600

    complete section 19 video

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,8 +14,11 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
 
-    @event.update(event_params)
-    redirect_to event_path(@event)
+    if @event.update(event_params)
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -24,9 +27,13 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.save()
 
-    redirect_to @event
+    if @event.save()
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+
   end
 
   def destroy
