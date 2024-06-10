fd2b483f383201b8de161a177a15861feb4a4f32
Colin Chauche <colin.chauche@gmail.com>
2024-06-10 14:46:21 -0600

    complete section 27 video

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_12_010122) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_10_162733) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_12_010122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_registrations_on_event_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "registrations", "events"
