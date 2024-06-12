409cc6a1a952841e75cc91e18951cb895d10339f
Colin Chauche <colin.chauche@gmail.com>
2024-06-12 15:27:04 -0600

    complete section 33p2 video

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -49,5 +50,10 @@ end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to events_url unless current_user?(@user)
   end
 end
